538a9f10bc14774c11b3b2515c5534aeb71f1330
Faa Diallo <faadiallop@gmail.com>
2024-02-25 02:50:08 -0800

    File comment & import statements

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
+#Creator: Faa Diallo
+#Date: 02/25/2024
+#This program creates a multi-threaded chat server. The corresponding
+#client.py file is able to send messages to this server.
+from _thread import *
+import threading
+
+def main():
+    pass
+
 if __name__ == "__main__":
-    print("Hello Tacos!")
+    main() 
