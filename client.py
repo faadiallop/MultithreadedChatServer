9a2bd8583e233dfa666ec9f06f05fa249e52e3eb
Faa Diallo <faadiallop@gmail.com>
2024-03-03 10:32:25 -0800

    Created a working client and server

--- client.py
+++ client.py
@@ -1,11 +1,57 @@
+#Creator: Faa Diallo
+#Date: 02/25/2024
+#This program is a client for a multi-threaded chat server. 
 import socket
+import select
+import sys
+
+udp_server_socket = None
 
 def main():
-	server_address_port = (127.0.0.1, 20001)
-	udp_client_socket =
+	""" Parameters: None 
+		Return: None
+		This function creates a socket to connect to the UDP loopback
+		server. It then either takes in input from the user to send
+		to the server or prints out messages sent from the server to
+		the client.
+	"""
+	global udp_server_socket
+	exit = False
+	buffer_size = 1024
+	server_address_port = ("127.0.0.1", 20001)
+	udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-	while True:
-		msg_from_client = "Here is a message the client sent to the server"
+	while not exit:
+		sys.stdout.write("> ")
+		sys.stdout.flush()
+
+		read_list = [sys.stdin, udp_server_socket] 
+
+		# Looks at the lists of file descriptors and returns when
+		# at least one of them is ready to be read.
+		ready_to_read, _, _ = select.select(read_list, [], [])
+
+
+		for fd in ready_to_read:
+			if fd == udp_server_socket:
+				# Reads in a message from the server and prints it.
+				message, address = udp_server_socket.recvfrom(buffer_size)
+				msg = f"{message.decode('UTF-8')}"
+				print(msg)
+			else:
+				# Reads in a message from the client and sends it to the server.
+				msg_from_client = sys.stdin.readline()
+				if msg_from_client.rstrip() == "exit": exit = True
+				udp_server_socket.sendto(bytes(msg_from_client, "UTF-8"), server_address_port)
+	
+def exit_program():
+	print("Exiting the UDP client.")
+	udp_server_socket.close()
+	sys.exit(0)
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+		exit_program()
+	except KeyboardInterrupt:
+		exit_program()
--- server.py
+++ server.py
@@ -6,30 +6,63 @@ import socket
 from _thread import *
 import threading
 
-def main():
+client_names = {}
 
+def main():
+    """ Parameters: None
+        Return: None
+        This function creates a loopback UDP socket and runs threads
+        for each of the client sockets that send messages to it.
+    """
     local_ip = "127.0.0.1"
     local_port = 20001 
     buffer_size = 1024
-    msg_from_server = "Message received on UDP Client and sending it again: "
+    print_lock = threading.Lock()
 
     #Creates an IPv4 UDP socket
-    udp_server_socket = socket.socket(family=socket.AF_INET, type=SOCK_DGRAM)
+    udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
     #Bind socket to IP and port
     udp_server_socket.bind((local_ip, local_port))
-
     print("UDP server up and listening")
 
-    while(True):
+    #Creates a thread for each connection to the server.
+    threading.Thread(target=client_socket_thread, 
+        args=(print_lock, udp_server_socket, buffer_size)).start()
+
+def client_socket_thread(print_lock, udp_server_socket, buffer_size):
+    """ Parameters: print_lock: Lock to prevent collisions in printing to stdout
+                    udp_server_socket: UDP socket for the server
+                    buffer_size: Maximum size block of text clients are able to send to server
+        Return: None
+        This function loops takes in input from any client and sends that input
+        to all of the clients sending messages to and from the server.
+    """
+    exit = False
+    while not exit:
         message, address = udp_server_socket.recvfrom(buffer_size)
+        message = message.decode('UTF-8').rstrip()
 
-        print(f"\nMessage from Client:{message.decode('UTF-8')}")
-        print(f"Client IP Address {address}")
+        in_dictionary = client_names.get(address)
+        client_names[address] = client_names.get(address, message) 
 
-        bytes_to_send = msg_from_server + message.decode('UTF-8')
+        if in_dictionary:
+            output = f"> {client_names[address]}: {message}"
+        else:
+            output = f"> {client_names[address]} has entered the server."
 
-        udp_server_socket.sendto(str.encode(bytes_to_send), address)
+        if message.rstrip() == "exit":
+            exit = True
+            output = f"> {client_names[address]} has exited the server."
+
+        print_lock.acquire()
+        print(output)
+        print_lock.release()
+
+        for ip_address in client_names:
+            udp_server_socket.sendto(bytes(output, 'UTF-8'), ip_address)
+            
+    client_names.pop(address) 
 
 if __name__ == "__main__":
     main() 
